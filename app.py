cc2b8e77b692212d2de31963b6fd9ce1dd72290f
Samir Poonawala <samirpoonawala@icloud.com>
2018-04-25 16:09:57 +0000

    first push of final stretch

--- app.py
+++ app.py
@@ -102,6 +102,23 @@ def stream(username=None):
     if username:
         template = 'user_stream.html'
     return render_template(template, stream=stream, user=user)
+
+@app.route("/follow/<username>")
+@login_required
+def follow(username):
+  try:
+    to_user = models.User.get(models.User.username**username)
+  except models.DoesNotExist:
+    pass
+  else:
+    try:
+      models.Relationship.create(from_user=g.user._get_current_object(), to_user=to_user)
+    except models.IntegrityError:
+      pass
+    else:
+      flash("You're now following {}!".format(to_user.username), "success")
+    return redirect(url_for("stream", username = to_user.username))
+
   
 if __name__ == "__main__":
   models.initialize()
--- models.py
+++ models.py
@@ -21,9 +21,16 @@ class User(UserMixin, Model):
         return Post.select().where(Post.user == self)
     
     def get_stream(self):
-        return Post.select().where(
-            (Post.user == self)
-        )
+        return Post.select().where((Post.user == self))
+    
+    def following(self):
+        """The users that we are following"""
+        return User.select().join(Relationship, on=Relationship.to_user).where(Relationship.from_user == self)
+      
+    def followers(self):
+        """Get users following the current user"""
+        return User.select().join(Relationship, on=Relationship.from_user).where(Relationship.to_user == self)
+      
         
     @classmethod
     def create_user(cls, username, email, password, admin=False):
@@ -49,9 +56,17 @@ class Post(Model):
     class Meta:
         database = DATABASE
         order_by = ('-timestamp',)
-            
+
+class Relationship(Model):
+  from_user = ForeignKeyField(User, related_name = "relationships")
+  to_user = ForeignKeyField(User, related_name = "related_to")
+  
+  class Meta:
+    database = DATABASE
+    indexes = ((("from_user", "to_user"), True))
+  
 
 def initialize():
     DATABASE.connect()
-    DATABASE.create_tables([User, Post], safe=True)
+    DATABASE.create_tables([User, Post, Relationship], safe=True)
     DATABASE.close()
